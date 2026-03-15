d7a242a6ff063df4c2d36390be3f7f7d9a92dea9
PhanManhTung-347 <phanmanhtung24112006@gmail.com>
2026-03-15 21:45:22 +0700

    PhanManhTungnopbaitap18

---
+++ PhanManhTung_Bai18/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+public class Student
+{
+    public string Name { get; set; }
+    public double Score { get; set; }
+    public Student(string name, double score)
+    {
+        Name = name;
+        this.Score = score;
+    }
+    public Student()
+    {
+    }
+}
+
+class Bai18
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Ho va ten: Phan Manh Tung || Msv: 2415053122347.");
+        Console.WriteLine("Bai 18: Quan ly diem sinh vien");
+        List<Student> students = new List<Student>();
+        students.Add(new Student("Van A", 8.5));
+        students.Add(new Student("Van B", 7.0));
+        students.Add(new Student("Van C", 9.0));
+        Console.WriteLine("Danh sach sinh vien:");
+        foreach (var student in students)
+        {
+            Console.WriteLine($"Ten: {student.Name} || Diem: {student.Score}");
+        }
+        Student hs = students[0];
+        foreach (var student in students)
+        {
+            if (student.Score > hs.Score)
+            {
+                hs = student;
+            }
+        }
+        Console.WriteLine($"Thong tin sinh vien co diem cao nhat: Name:{hs.Name} || Score:{hs.Score}");
+    }
+}
