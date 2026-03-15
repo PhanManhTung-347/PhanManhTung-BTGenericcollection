169fb9a0199df4ee237d5b9df4da4da1865cfa45
PhanManhTung-347 <phanmanhtung24112006@gmail.com>
2026-03-15 21:39:45 +0700

    PhanManhTungnopbaitap15

---
+++ PhanManhTung_Bai15/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Bai15
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Ho va ten: Phan Manh Tung || Msv: 2415053122347.");
+        Console.WriteLine("Bai 15: Tu dien Anh - Viet (Dictionary)");
+        Dictionary<string, string> tuDien = new Dictionary<string, string>();
+        tuDien.Add("Hello", "Xin chao");
+        tuDien.Add("Student", "Sinh vien");
+        tuDien.Add("Teacher", "Giao vien");
+        tuDien.Add("Book", "Quyen sach");
+        tuDien.Add("Computer", "May tinh");
+        string y="";
+        do
+        {
+            Console.WriteLine("Nhap tu tieng Anh can tra nghia: ");
+            string tuKhoa = Console.ReadLine();
+            if (tuDien.TryGetValue(tuKhoa, out string nghiaViet))
+            {
+                Console.WriteLine($"Nghia cua tu '{tuKhoa}' la: {nghiaViet}");
+            }
+            else
+            {
+                Console.WriteLine($"He thong chua cap nhat tu: {tuKhoa} nay. Vui long thu lai!");
+            }
+            Console.WriteLine("Nhap y de tiep tuc tra hoac bat ky de thoat:");
+            y=Console.ReadLine();
+            
+        }
+        while (y == "y" || y == "Y");
+        Console.WriteLine("\n--- Cac tu dang co trong he thong ---");
+        foreach (KeyValuePair<string, string> item in tuDien)
+        {
+            Console.WriteLine($"{item.Key}: {item.Value}");
+        }
+        Console.WriteLine("Cam on da su dung chuong trinh!");
+    }
+}
