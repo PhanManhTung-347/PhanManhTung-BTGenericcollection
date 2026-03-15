a6f0665cc84b2904c523f93223d5a4cf32404879
PhanManhTung-347 <phanmanhtung24112006@gmail.com>
2026-03-15 21:22:51 +0700

    PhanManhTungnopbaitap11

---
+++ PhanManhTung_Bai11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+public class Student
+{
+    public int Id { get; set; } 
+    public string Name { get; set; }
+    public Student(int Id, string Name)
+    {
+        this.Id = Id;
+        this.Name = Name;
+    }
+}
+class Bai11
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Ho va ten: Phan Manh Tung || Msv: 2415053122347.");
+        Console.WriteLine("Bai 11: Luu danh sach sinh vien");
+        List<Student> students = new List<Student>();
+        students.Add(new Student(1, "Van A"));
+        students.Add(new Student(2, "Van B"));
+        students.Add(new Student(3, "Van C"));
+        students.Add(new Student(4, "Van D"));
+        students.Add(new Student(5, "Van E"));
+        Console.WriteLine($"Danh sach sinh vien {students.Count} sinh vien:");
+        foreach (Student student in students)
+        {
+            Console.WriteLine($"ID:{student.Id} || Name:{student.Name}");
+        }
+    }
+}
